66f21b722581b211572754f9f0885cc658c87111
gversmee <34459351+gversmee@users.noreply.github.com>
2018-05-08 16:47:01 -0400

    better

--- R/flatten.tree.R
+++ R/flatten.tree.R
@@ -1,29 +1,20 @@
 #' @author Gregoire Versmee, Mikael Dusenne, Laura Versmee
 
-
 flatten.tree <- function(env, path, token, verbose = FALSE)  {
-
-
-
-  fetchNode <- function(e)  content.get(paste0(env, "/rest/v1/resourceService/path", gsub("\\?", "%3F", e)), token)
 
   f <- function(l)  {
     unlist(sapply(l, function(e)  {
-                    node <- fetchNode(e)
+                    node <- content.get(paste0(env, "/rest/v1/resourceService/path", gsub("\\?", "%3F", e)), token)
                                   if(length(node) == 0)  {
                                     if (verbose)  message(e)
                                     return(e)
                                   } else {
                                     if (!is.null(node$errorType) | !is.null(node$status)) {
-                                      message(paste0('!!!There is an issue in the database with the path: "', e, '"\n-> discarding path. Please contact the developpers regarding this issue!!!\n'))
-                                    } else {
-                                      return(f(sapply(node, function(n) n$pui)))
-                                    }
+                                      message(paste0('\n   !!!There is an issue in the database with the path: "', e, '"\n   -> discarding path. Please contact the developpers regarding this issue!!!\n'))
+                                    } else  return(f(sapply(node, function(n) n$pui)))
                                   }
                                 },
                   USE.NAMES = FALSE))
   }
   return(as.vector(f(path)))
 }
-
-
