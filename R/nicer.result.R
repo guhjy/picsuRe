935e43aec803987c4cdffbc66bedeb976bad9de1
gversmee <34459351+gversmee@users.noreply.github.com>
2018-07-28 09:43:16 -0400

    debug cat vs num

--- R/nicer.result.R
+++ R/nicer.result.R
@@ -14,19 +14,19 @@ nicer.result <- function(result, allpaths, verbose = FALSE)  {
   final <- result[1]
   cnames <- c("Patient_id")
 
-  for (i in 1:length(groups2))  {
-    subdf <- result[-1][groups == groups2[i]]
+  for (e in groups2)  {
+    subdf <- result[-1][groups == e]
     if (length(unique(subdf[,1])) <= 2  & any(is.na(unique(subdf[,1]))))  {
       subdf[is.na(subdf)] <- ""
       final <- cbind(final, as.factor(apply(subdf, 1, paste0, collapse = "")))
-      if (!is.null(names(groups2)) & names(groups2[i]) != "")  {
-        cnames <- c(cnames, names(groups2[i]))
+      if (!all(is.null(names(groups2)))) {
+          if (names(e) != "") cnames <- c(cnames, names(e)) else  cnames <- c(cnames, basename(dirname(colnames(subdf)[1])))
       } else  cnames <- c(cnames, basename(dirname(colnames(subdf)[1])))
 
     } else {
       final <- cbind(final, subdf)
-      if (!is.null(names(groups[groups == groups2[i]])))  {
-        cnames <- c(cnames, names(groups[groups == groups2[i]]))
+      if (!is.null(names(groups[groups == e])))  {
+        cnames <- c(cnames, names(groups[groups == e]))
       } else  cnames <- c(cnames, basename(colnames(subdf)))
     }
   }
